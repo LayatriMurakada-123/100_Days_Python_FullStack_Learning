2e9bb749e080f485da45a3de3d442fdb0876da58
LayatriMurakada-123 <layatrimurakada@gmail.com>
2026-06-28 17:48:39 +0530

    Add files via upload

---
+++ PythonDay1.py
@@ -0,0 +1,109 @@
+'''Procedural language 
+-------------------
+--> This follows a step-by-step approach , where code is structure into procedures such as function or routines....
+
+ex --> C
+
+OOP's
+-------
+-->This is based on concepts of objects and class..
+
+ex-->python , java
+
+Python
+------
+1.dynamically typed
+-------------------
+-->Python knows the type of data we are passing to the variable...(no need to mention any kind of data)
+
+2.interpreter
+-------------
+-->Python execute code line-by-line , if any error it will stop the execution in that and before will gives the output..
+
+eg
+--
+print(89+67)
+num = 89
+num_2 =67
+total_ = num + num_2
+print(id(num))
+print(id(num_2))
+print(id(total_))
+
+3.High_level
+
+
+1991--> Guide Van Russum
+
+Why using python
+----------------
+-->easy syntax
+-->cross-platform
+-->wide application
+-->Huge library support
+
+
+
+Tokens
+------
+1.Keywords
+----------
+-->these are reserved words in python
+ex
+--
+if , else , for , while
+
+2.Identifiers
+-------------
+-->Names given to variables , function , class
+
+3.Literals
+----------
+-->89 , "hello" , 4.56
+
+4.Operators
+-----------
+--> + _ * /
+
+5.Punctuators
+-------------
+--> () , [] , {}
+
+
+
+Rules of variable
+-----------------
+--> these can use  - A-Z , a-z , _
+ex
+--
+num_1=7
+num=8
+Num_2=9
+_num=2
+
+--> these cannot use  - Special char , number , space
+$num = 0
+su@m = 9
+s o = 89
+2num = 9
+
+
+Comments
+--------
+1.Single line comment ---> #
+2.Multi line comment  ---> ''' ''' , """ """
+
+
+num = 9
+if num % 2 == 0: #checking a number is even or odd
+   print("Even")
+else:
+   print("odd")
+
+Boolean
+-------
+1.True
+2.False
+
+'''
+

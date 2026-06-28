2892b9a16a5d0de00d8f80da5615d74bcc51d4c7
LayatriMurakada-123 <layatrimurakada@gmail.com>
2026-06-28 17:49:11 +0530

    Add files via upload

---
+++ PythonDay2.py
@@ -0,0 +1,117 @@
+'''                           Operators in Python
+                            --------- -- ------
+
+1.Arithmetic Operator
+ ----------- ---------
+ --> +,-,*,/,%,//,**(exponential)
+ Ex : a=2
+      b=3
+      print(a+b)
+      print(a-b)
+      print(a*b)
+      print(a/b)
+      print(a%b)
+      print(a**b)
+      print(a//b)
+      
+2.Assignment Operator
+  ---------- --------
+  --> =,+=(increment operator),-=(decrement operator),*=,/=
+  
+3.Comparision Operator
+  ----------- --------
+  --> ==,!=,<=,>=,<,>
+  
+4.Logical Operator
+  ------- --------
+  --> and ---> Returns true if both are true
+        eg:- a = 20
+             b = 7
+             c = 89
+             print(a>=b and b<=c)
+      or  ---> Return true if any one is correct
+      not --->reverser the output
+      
+5.Identity Operator
+  -------- --------
+  --> is --> same object
+        ex:- a = 20
+             b = 7
+             print(a == b)
+             print(a is b)
+  --> is not -->  not the same object
+
+6.Membership Operator
+  ---------- --------
+  --> in     ---> whether it inside
+  
+           eg:-a = [1,2,3,4,5]
+               print(2 in a)
+      not in ---> whether it is not inside
+      
+7.Bitwise Operator
+  ------- --------
+  --> &,<<,>>,|,^
+     eg:- print(5&3)
+          print(5|3)
+          print(5<<3)
+          print(5>>3)
+
+          print(5^3)
+
+
+eg:-
+
+Arithmetic Operator:-
+---------- --------
+a=5
+b=6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+Assignment Operator:-
+---------- --------
+x+=7
+x-=7
+x*=7
+x=7
+x/=7
+print(x)
+
+Comparision Operator:-
+----------- --------
+a=8
+b=9
+print(a==b)
+print(a!=b)
+print(a<=b)
+print(a>=b)
+print(a>b)
+print(a<b)
+
+Logical OPerator:-
+------- --------
+a=8
+b=6
+c=4
+print(a>b and b>c)
+print(a>b or b<c)
+
+Identity Operator:-
+-------- --------
+a=10
+b=12
+print(a==b)
+print(a is b)
+
+Membership operator:-
+---------- -------
+a=[1,2,3,4,5]
+
+print[7 in a]
+

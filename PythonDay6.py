b2d63ff4963c67e81f98e204105f88b3dd5eec73
LayatriMurakada-123 <layatrimurakada@gmail.com>
2026-06-29 12:34:17 +0530

    Add files via upload

---
+++ PythonDay6.py
@@ -0,0 +1,67 @@
+'''
+
+Type Conversion : This is Process of converting one data type into another...
+
+Int --> string ;float
+eg  num = 89
+--  num_2 = float(num)
+    print(num_2)
+    print(type(num))
+    so = str(num)
+    print(type(so))
+
+
+Float : String ; Integer
+eg     num = 8.9
+--     how = str(num)
+       print(how)
+       print(type(how))
+       num_2 = int(num)
+       print(num_2)
+
+String : integer ; string
+eg    hai = "78"
+--    num = int(hai)
+      print(num+10)
+      hello = "67.89"
+      num_2 = float(hello)
+      print(num_2 + num)
+
+      any_ = "python"
+      con_ = list(any_)
+      print(con_)
+      con_2 = tuple(any_)
+      print(con_2)
+
+List : integer ; float ; list ;tuple
+eg      var_ = ['p','y','t','h','o','n']
+        text_ = "".join(var_)
+        print(text_)
+        var_ = ['p','y','t','h','o','n']
+        some = tuple(var_)
+        print(some)
+
+        pyth_ = [('a',1),('b',8)]
+        conver = dict(pyth_)
+        print(conver)
+
+        fam = ('1','2','3','4')
+        text_2 = "".join(fam)
+        print(text_2)
+
+Tuple : String ; Lists
+
+
+
+Built-in functions
+-------- ----------
+str()
+float()
+int()
+list()
+tuple()
+dict()
+
+
+
+'''
